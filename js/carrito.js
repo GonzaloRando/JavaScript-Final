da029ac5d8d223c91b64f48965de0cf5a05e2596
Gonzalo Rando <randosgonzalo@gmail.com>
2021-12-21 20:30:56 -0300

    Desafío AJAX

--- js/carrito.js
+++ js/carrito.js
@@ -1,9 +1,23 @@
+// VARIABLES
+const vinos = [];
+const carrito = [];
+const productos = document.getElementById('shop')
+
+// CARGA DE PRODUCTOS
+$.getJSON('../data/productos.json', function (data){
+    console.log(data)
+    data.forEach(elemento => vinos.push(elemento))
+    mostrarProductos();
+    
+})
+
+
 class Vino {
-    constructor(id, cepa, cosecha, info, imagen, stock, cantidad) {
+    constructor(id, cepa, cosecha, precio, imagen, stock, cantidad) {
         this.id = id;
         this.cepa = cepa;
         this.cosecha = cosecha;
-        this.info = info;
+        this.precio = precio;
         this.imagen = imagen;
         this.stock = stock;
         this.cantidad = cantidad || 0
@@ -14,29 +28,15 @@ class Vino {
     }
 }
 
-const vinos = [];
-const carrito = [];
 
-const vino1 = new Vino(1, "Malbec Salta", "2021", "Envuelve los sentidos con un exquisito bouquet de ciruela dulce y picante.", "../Multimedia/Imagenes/PortfolioVinos/MalbecSalta.jpg", 10);
-const vino2 = new Vino(2, "Cabernet Sauvignon", "2019", "De color púrpura intenso con aromas brillantes de frutos rojos y especias.", "../Multimedia/Imagenes/PortfolioVinos/CabernetSauv.jpg", 5);
-const vino3 = new Vino(3, "Malbec + Tannat", "2020", "Posee una alta estructura  y acidez que se equilibran en un final persistente.", "../Multimedia/Imagenes/PortfolioVinos/MalbecTannat.jpg", 4);
-const vino4 = new Vino(4, "Malbec Premium", "2015", "Calienta el paladar con notas de moras, arándanos y lavanda.", "../Multimedia/Imagenes/PortfolioVinos/MalbecPremium.jpg", 6);
-const vino5 = new Vino(5, "Rosé", "2021", "Es un vino de cuerpo medio que seducirá tus sentidos con sus aromas florales.", "../Multimedia/Imagenes/PortfolioVinos/RoseMendoza.jpg", 8);
-const vino6 = new Vino(6, "Torrontés", "2021", "Estalla con aromas tropicales de piña, melocotón y flor de azahar del naranjo.", "../Multimedia/Imagenes/PortfolioVinos/TorrontesSalta.jpg", 3);
-const vino7 = new Vino(7, "Malbec Mendoza", "2021", "Tienta los sentidos con un aromático bouquet de café y especias.", "../Multimedia/Imagenes/PortfolioVinos/MalbecMendoza.jpg", 7);
-
-
-vinos.push(vino1, vino2, vino3, vino4, vino5, vino6, vino7);
-
-const productos = document.getElementById('shop')
-
+// FUNCION PARA AJAX Y DOM CON JQUERY
+function mostrarProductos(){
 for (const vino of vinos) {
     $('#shop').append(`
     <div class="card-vinos" style="width: 18rem;">
         <img src="${vino.imagen}" class="card-img-top" style="width: 50px;" alt="...">
         <div class="card-body">
             <h5 class="card-title">${vino.cepa}</h5>
-            <p class="card-text">${vino.info}</p>
         </div>
     <ul class="list-group list-group-flush">
         <li class="list-group-item"> Cosecha: ${vino.cosecha}</li>
@@ -47,6 +47,7 @@ for (const vino of vinos) {
     </div>
     `)
     document.getElementById(`${vino.id}`).addEventListener(`click`, () => comprarVino(vino))
+}
 }
 
 
@@ -76,7 +77,7 @@ function cargarLocalStorage(){
     localStorage.setItem('carrito', carritoStorage)
     if(carritoStorage){
         for(let i = 0; i < carritoStorage.length; i++){
-            carrito.push(new Vino(carritoStorage[i].id, carritoStorage[i].cepa, carritoStorage[i].cosecha, carritoStorage[i].info, carritoStorage[i].imagen, carritoStorage[i].stock, carritoStorage[i].cantidad))
+            carrito.push(new Vino(carritoStorage[i].id, carritoStorage[i].cepa, carritoStorage[i].cosecha, carritoStorage[i].precio, carritoStorage[i].info, carritoStorage[i].imagen, carritoStorage[i].stock, carritoStorage[i].cantidad))
         }
         let total = 0;
     for (let i=0; i<carrito.length;i++){
