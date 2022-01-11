a1036247c1c84de1f7685e4da2360f28ee6e008c
Gonzalo Rando <randosgonzalo@gmail.com>
2022-01-10 23:58:10 -0300

    Entrega Proyecto Final

--- js/carrito.js
+++ js/carrito.js
@@ -1,95 +1,44 @@
-// VARIABLES
-const vinos = [];
-const carrito = [];
-const productos = document.getElementById('shop')
+let carrito = []
+let dataCarrito = localStorage.getItem("carrito");
 
-// CARGA DE PRODUCTOS
-$.getJSON('../data/productos.json', function (data){
-    data.forEach(elemento => vinos.push(elemento))
-    mostrarProductos();
-    
-})
-
-
-class Vino {
-    constructor(id, cepa, cosecha, precio, imagen, stock, cantidad) {
-        this.id = id;
-        this.cepa = cepa;
-        this.cosecha = cosecha;
-        this.precio = precio;
-        this.imagen = imagen;
-        this.stock = stock;
-        this.cantidad = cantidad || 0
-
-    }
-    aumentarCantidad() {
-        this.cantidad++
-    }
+if(dataCarrito) {
+    carrito = JSON.parse(dataCarrito)
+    mostrarCarrito()
 }
-
-
-// FUNCION PARA AJAX Y DOM CON JQUERY
-function mostrarProductos(){
-for (const vino of vinos) {
-    $('#shop').append(`
-    <div class="card-vinos" style="width: 18rem;">
-        <img src="${vino.imagen}" class="card-img-top" style="width: 50px;" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${vino.cepa}</h5>
+function mostrarCarrito(){
+    document.getElementById("pintarProductos").innerHTML = ""
+    for(const vino of carrito){
+        $('#pintarProductos').append( `
+        <div class="card-vinos" style="width: 18rem;">
+            <img src="${vino.imagen}" class="card-img-top" style="width: 50px;" alt="vino.cepa">
+            <div class="card-body">
+                <h5 class="card-title">${vino.cepa}</h5>
+            </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"> Cosecha: ${vino.cosecha}</li>
+            <p class="card-text">Unidades seleccionadas(${vino.cantidad})</p>
+            <p class="card-text">Productos disponibles(${vino.stock})</p>
+            <p class="card-text"><b>$ ${vino.precio}</b></p>
+            <button id="${vino.id}eliminar" class="btn btn-primary delete">🗑️</button>
+        </ul>
         </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item"> Cosecha: ${vino.cosecha}</li>
-        <li class="list-group-item">
-        <button id=${vino.id} class="btn btn-primary reservar">Comprar</button>
-        </li>
-    </ul>
-    </div>
-    `)
-    document.getElementById(`${vino.id}`).addEventListener(`click`, () => comprarVino(vino))
-}
+        `)
+
+        document.getElementById(`${vino.id}eliminar`).addEventListener('click', () => eliminar(vino))
+    }
+
 }
 
 
-
-function comprarVino(producto){
-    let compra = carrito.find(el=> el.nombre === producto.nombre)
-    if(compra){
-        if(compra.cantidad < producto.stock){
-            compra.aumentarCantidad();
-        }
-        
+function eliminar(producto){
+    const eliminado = carrito.find(el => el.id === producto.id)
+    if(eliminado.cantidad > 1 ){
+        eliminado.cantidad--
     }else{
-        carrito.push(producto);
-        producto.aumentarCantidad();
+        carrito = carrito.filter(el => el.id !== producto.id)
     }
-    let total = 0;
-    for(let i=0; i<carrito.length;i++){
-        total += carrito[i].cantidad;
-    }
-    const compraProductos = document.getElementById('compraProductos');
-    compraProductos.innerHTML = total;
     localStorage.setItem('carrito', JSON.stringify(carrito));
+    mostrarCarrito()
+
 }
-
-function cargarLocalStorage(){
-    let carritoStorage = JSON.parse(localStorage.getItem('carrito'))
-    localStorage.setItem('carrito', carritoStorage)
-    if(carritoStorage){
-        for(let i = 0; i < carritoStorage.length; i++){
-            carrito.push(new Vino(carritoStorage[i].id, carritoStorage[i].cepa, carritoStorage[i].cosecha, carritoStorage[i].precio, carritoStorage[i].info, carritoStorage[i].imagen, carritoStorage[i].stock, carritoStorage[i].cantidad))
-        }
-        let total = 0;
-    for (let i=0; i<carrito.length;i++){
-        total += carrito[i].cantidad;
-    }
-    const compraProductos = document.getElementById('compraProductos');
-    compraProductos.innerHTML = total;
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-    }
-}
-
-cargarLocalStorage();
-
-
-
 
--- js/productos.js
+++ js/productos.js
@@ -1,1 +1,97 @@
-$
+// VARIABLES
+const vinos = [];
+const carrito = [];
+const productos = document.getElementById('shop')
+
+// CARGA DE PRODUCTOS
+$.getJSON('../data/productos.json', function(datos){
+    if(datos){
+        for(const product of datos){
+            vinos.push(new Vino (product.id, product.cepa, product.cosecha, product.precio, product.imagen, product.stock, product.cantidad))
+        }
+        mostrarProductos();
+    }
+})
+
+
+class Vino {
+    constructor(id, cepa, cosecha, precio, imagen, stock, cantidad) {
+        this.id = id;
+        this.cepa = cepa;
+        this.cosecha = cosecha;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.stock = stock;
+        this.cantidad = cantidad || 0
+
+    }
+    aumentarCantidad() {
+        this.cantidad++
+    }
+}
+
+
+// FUNCION PARA AJAX Y DOM CON JQUERY
+function mostrarProductos(){
+for (const vino of vinos) {
+    $('#shop').append(`
+    <div class="card-vinos" style="width: 18rem;">
+        <img src="${vino.imagen}" class="card-img-top" style="width: 50px;" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${vino.cepa}</h5>
+        </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"> Cosecha: ${vino.cosecha}</li>
+        <li class="list-group-item">
+        <button id=${vino.id} class="btn btn-primary reservar">Comprar</button>
+        </li>
+    </ul>
+    </div>
+    `)
+    document.getElementById(`${vino.id}`).addEventListener(`click`, () => comprarVino(vino))
+}
+}
+
+
+
+function comprarVino(producto) {
+    let compra = carrito.find(el => el.id === producto.id)
+    if (compra) {
+        if (compra.cantidad < producto.stock) {
+            compra.aumentarCantidad();
+        }
+    } else {
+        producto.aumentarCantidad();
+        carrito.push(producto);
+    }
+
+    let total = 0;
+    for (let i = 0; i < carrito.length; i++) {
+        total += carrito[i].cantidad;
+    }
+    const compraTotal = document.getElementById('compraProductos');
+    compraTotal.innerHTML = total;
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+
+function cargarLocalStorage() {
+    let carritoStorage = JSON.parse(localStorage.getItem('carrito'))
+    
+    if (carritoStorage) {
+        for (let i = 0; i < shop.length; i++) {
+            carrito.push(new Vino (carritoStorage[i].id, carritoStorage[i].cepa, carritoStorage[i].cosecha, carritoStorage[i].precio, carritoStorage[i].imagen, carritoStorage[i].stock, carritoStorage[i].cantidad))
+        }
+        let total = 0;
+        for (let i = 0; i < carrito.length; i++) {
+            total += carrito[i].cantidad;
+        }
+        const compraProductos = document.getElementById('compraProductos');
+        compraProductos.innerHTML = total;
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        console.log(carrito)
+    }
+}
+cargarLocalStorage(); 
+
